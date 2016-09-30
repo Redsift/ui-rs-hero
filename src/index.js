86dc1680ddfaba729fbf05d55dc020ccfd2a3a77
Martin Hecher <martin@redsift.io>
2016-09-30 08:58:45 +0100

    added conditional webcomponents polyfill

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import registerHeroElement from './js/hero-element.js';
 import RedsiftHero from './js/hero.js';
 
-// register the element per default:
-registerHeroElement();
+(function() {
+  if ('registerElement' in document
+      && 'import' in document.createElement('link')
+      && 'content' in document.createElement('template')) {
+    // platform is good!
+    // register the element per default:
+    registerHeroElement();
+  } else {
+    // polyfill the platform!
+    var e = document.createElement('script');
+    e.src = '/js/CustomElements.min.js';
+    document.body.appendChild(e);
+
+    window.addEventListener('WebComponentsReady', function(e) {
+      // register the element per default:
+      registerHeroElement();
+    });
+  }
+})();
 
 export {
   registerHeroElement,
